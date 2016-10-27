98734588ff53be7a11e10a4e13524ab2a9b102c2
Meman <excellent.nomad@gmail.com>
2016-10-27 03:31:19 -0500

    Replaced with some of ES6 features

--- src/app.component.js
+++ src/app.component.js
@@ -1,15 +1,10 @@
-(function(app) {
-  var Component = ng.core.Component;
+import { Component } from '@angular/core';
 
-  @Component({
-    selector: 'my-app',
-    template: `
-      <h1>Random Quote</h1>
-      <random-quote></random-quote>
-    `
-  })
-  class AppComponent { }
-
-  app.AppComponent = AppComponent;
-
-})(window.app || (window.app = {}));
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Random Quote</h1>
+    <random-quote></random-quote>
+  `
+})
+export class AppComponent { }
--- src/app.module.js
+++ src/app.module.js
@@ -1,17 +1,13 @@
-(function(app) {
-  var NgModule = ng.core.NgModule;
-  var BrowserModule = ng.platformBrowser.BrowserModule;
-  var QuoteService = app.QuoteService;
-  var RandomQuoteComponent = app.RandomQuoteComponent;
-  var AppComponent = app.AppComponent;
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { QuoteService} from './quote.service';
+import { RandomQuoteComponent } from './random-quote.component';
+import { AppComponent } from './app.component';
 
-  @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent, RandomQuoteComponent],
-    providers: [QuoteService],
-    bootstrap: [AppComponent]
-  })
-  class AppModule {}
-  app.AppModule = AppModule;
-
-})(window.app || (window.app = {}));
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, RandomQuoteComponent],
+  providers: [QuoteService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
